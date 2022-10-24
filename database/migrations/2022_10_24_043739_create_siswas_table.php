7c1f46b455883baf846932855df8e4bd4e7c60d8
dhomanhuri <dhomanhuri@gmail.com>
2022-10-24 12:41:26 +0700

    first commit

---
+++ database/migrations/2022_10_24_043739_create_siswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nis');
+            $table->string('nama');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('alamat');
+            $table->string('kelas_id')->constrained('kelas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
---
+++ database/migrations/2022_10_24_044353_create_bukus_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idbuku');
+            $table->string('penulis');
+            $table->date('tahun');
+            $table->string('judul');
+            $table->string('kota');
+            $table->string('penerbit');
+            $table->string('cover');
+            $table->text('sinopsis');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
